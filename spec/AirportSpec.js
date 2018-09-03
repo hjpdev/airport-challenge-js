ffcd117678c4740f5b2f6a9f66dbd81fdc58569c
Andrew Wood <andrew.wood.dev@outlook.com>
2018-09-03 15:51:57 +0100

    take off planes

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -13,4 +13,15 @@ describe("Airport", function() {
     airport.land(plane);
     expect(airport._planes).toEqual([plane]);
   });
+  it("should be able to land two planes", function() {
+    airport.land(plane);
+    airport.land(plane);
+    expect(airport._planes).toEqual([plane, plane]);
+  });
+  it("should be able to take off a plane", function() {
+    airport.land(plane);
+    airport.takeoff(plane);
+    expect(airport._planes).toEqual([]);
+  });
+
 });
--- src/Airport.js
+++ src/Airport.js
@@ -2,7 +2,10 @@ function Airport() {
   this._planes = []
 }
 
-
 Airport.prototype.land = function(plane) {
-  this._planes = [plane];
+  this._planes.push(plane);
+}
+
+Airport.prototype.takeoff = function(plane) {
+  this._planes.pop();
 }
